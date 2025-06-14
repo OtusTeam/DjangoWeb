2deb0607d4c24fc1dfebc028d552785d240f7354
Alexandr <alexandr.ignatev1996@gmail.com>
2025-06-14 14:46:25 +0700

    Add lesson 11: Django settings

---
+++ lessons/lesson.11/shop_project/shop_app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from shop_app.models import Product, Category
+import textwrap
+
+
+# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'pk', 'title', 'categories_list', 'description_short', 'price'
+    list_display_links = ('pk', 'title')
+
+    def categories_list(self, obj:Product):
+        return ', '.join([category.title for category in obj.categories.all()])
+    
+    def description_short(self, obj:Product):
+        return textwrap.shorten(obj.description, width=20, placeholder=' <...>')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'pk', 'title', 'description' 
---
+++ lessons/lesson.11/shop_project/shop_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+# Create your models here.
+class Product(models.Model):
+    class Meta:
+        ordering = ['pk']
+        verbose_name_plural = 'Товары'
+
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название')
+    description = models.TextField(null=False, blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    categories = models.ManyToManyField(
+        to='Category',
+        related_name='products',
+        verbose_name='Категория'
+    )
+
+
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Категории'
+        ordering = ['pk']
+    
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название категории')
+    description = models.TextField(null=False, blank=True, verbose_name='Описание категории')
+
+    def __str__(self):
+        return self.title
